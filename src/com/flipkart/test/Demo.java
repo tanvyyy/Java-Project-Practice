c9d0396b06dbaee52a0c2cdcdd46fef312770e0c
tanvi.tanya <tanvi.tanya@FK1089-215037L.local>
2024-06-25 15:32:25 +0530

    first commit

---
+++ src/com/flipkart/test/Demo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.test;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Program");
+	}
+
+}
